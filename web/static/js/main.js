d29a0f7bd394434d52744c0e9603c0016c706e07
Lucas Bitello da Silva <lucasbitellodasilva@outlook.com>
2023-05-14 17:44:39 -0300

    Aumentado timeout para o fetch no js.

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,3 +1,3 @@
-const URL_BASE = 'http://34.136.247.177'
+const URL_BASE = 'http://127.0.0.1:8000'
 const NIVEL_API_ATUAL = "/api/v1"
-const URL_BASE_API = 'http://34.136.247.177' + NIVEL_API_ATUAL
+const URL_BASE_API = 'http://127.0.0.1:8000' + NIVEL_API_ATUAL
