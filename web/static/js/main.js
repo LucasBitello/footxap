cc627063b1deb80ee69fa90d4040880b3145c542
Lucas Bitello da Silva <lucasbitellodasilva@outlook.com>
2023-05-15 01:14:51 -0300

    Comitado print para debug na VM, problema em formato nos caracteres interpretado entre windows e linux.

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,3 +1,3 @@
-const URL_BASE = 'http://34.136.247.177'
+const URL_BASE = 'http://10.0.0.147:8000'
 const NIVEL_API_ATUAL = "/api/v1"
-const URL_BASE_API = 'http://34.136.247.177' + NIVEL_API_ATUAL
+const URL_BASE_API = 'http://10.0.0.147:8000' + NIVEL_API_ATUAL
